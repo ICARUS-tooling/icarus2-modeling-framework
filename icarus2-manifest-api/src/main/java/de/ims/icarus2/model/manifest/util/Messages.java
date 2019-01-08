285a6e7118162cb25d76dd198a863cc0db07712e
Markus Gärtner <mcgaerty@ikarus-systems.net>
2019-01-08 10:31:19 +0100

    Make method names in Messages more concise.

--- icarus2-manifest-api/src/main/java/de/ims/icarus2/model/manifest/util/Messages.java
+++ icarus2-manifest-api/src/main/java/de/ims/icarus2/model/manifest/util/Messages.java
@@ -64,70 +64,68 @@ public class Messages {
 		return msg;
 	}
 
-	//TODO rename methods to shorten them and remove the "message" suffix
-
 	public static String mismatch(String msg, Object expected, Object provided) {
 		return String.format("%s: expected %s - got %s", ensureMsg(msg), expected, provided); //$NON-NLS-1$
 	}
 
-	public static String alreadySetMessage(String msg, Object present, Object given) {
+	public static String alreadySet(String msg, Object present, Object given) {
 		return String.format("%s: %s already present - cant set %s", ensureMsg(msg), present, given); //$NON-NLS-1$
 	}
 
-	public static String missingMappingMessage(String msg, LayerManifest sourceLayer, LayerManifest targetLayer) {
+	public static String missingMapping(String msg, LayerManifest<?> sourceLayer, LayerManifest<?> targetLayer) {
 		return String.format("%s: no mapping from %s to %s", ensureMsg(msg), sourceLayer.getId(), targetLayer.getId()); //$NON-NLS-1$
 	}
 
-	public static String sizeMismatchMessage(String msg, long expected, long size) {
+	public static String sizeMismatch(String msg, long expected, long size) {
 		return String.format("%s: expected size %d - got %d", ensureMsg(msg), _long(expected), _long(size)); //$NON-NLS-1$
 	}
 
-	public static String offsetMismatchMessage(String msg, long expected, long index) {
+	public static String offsetMismatch(String msg, long expected, long index) {
 		return String.format("%s: expected offset %d - got %d", ensureMsg(msg), _long(expected), _long(index)); //$NON-NLS-1$
 	}
 
-	public static String illegalOffsetMessage(String msg, long index) {
+	public static String illegalOffset(String msg, long index) {
 		return String.format("%s: unexpected offset %d", ensureMsg(msg), _long(index)); //$NON-NLS-1$
 	}
 
-	public static String outOfBoundsMessage(String msg, long index, long min, long max) {
+	public static String outOfBounds(String msg, long index, long min, long max) {
 		return String.format("%s: value %d out of bounds [%d,%d]", ensureMsg(msg), _long(index), _long(min), _long(max)); //$NON-NLS-1$
 	}
 
-	public static String insufficientEdgesMessage(String msg, Object node, long required, long count) {
+	public static String insufficientEdges(String msg, Object node, long required, long count) {
 		return String.format("%s: insufficient edge count of %d at node %s - got %d", ensureMsg(msg), _long(required), node, _long(count)); //$NON-NLS-1$
 	}
 
-	public static String edgesOverflowMessage(String msg, Object node, long max, long count) {
+	public static String edgesOverflow(String msg, Object node, long max, long count) {
 		return String.format("%s: edge count %d exceeds limit of %d at node %s", ensureMsg(msg), _long(count), _long(max), node); //$NON-NLS-1$
 	}
 
-	public static String indexOutOfBoundsMessage(String msg, Object container, long size, long index) {
+	public static String indexOutOfBounds(String msg, Object container, long size, long index) {
 		return String.format("%s: index %d exceeds size %d of container %s", ensureMsg(msg), _long(index), _long(size), container); //$NON-NLS-1$
 	}
 
-	public static String indexOutOfBoundsMessage(String msg, long min, long max, long index) {
+	public static String indexOutOfBounds(String msg, long min, long max, long index) {
 		return String.format("%s: index %d exceeds range %d to %d", ensureMsg(msg), _long(index), _long(min), _long(max)); //$NON-NLS-1$
 	}
 
-	public static String indexNegativeMessage(String msg, Object container, long index) {
+	public static String indexNegative(String msg, Object container, long index) {
 		return String.format("%s: negative index for container %s: %d", ensureMsg(msg), container, _long(index)); //$NON-NLS-1$
 	}
 
-	public static String foreignItemMessage(String msg, Object container, Object item) {
+	public static String foreignItem(String msg, Object container, Object item) {
 		return String.format("%s: item %s is not a valid member of the following container: %s", ensureMsg(msg), item, container); //$NON-NLS-1$
 	}
 
-	public static String foreignContainerMessage(String msg, Object expected, Object actual) {
+	public static String foreignContainer(String msg, Object expected, Object actual) {
 		return String.format("%s: foreign container: expected %s - got %s", ensureMsg(msg), expected, actual); //$NON-NLS-1$
 	}
 
-	public static String nonOverlappingIntervalsMessage(String msg, long expectedBegin, long expectedEnd, long actualBegin, long actualEnd) {
+	public static String nonOverlappingIntervals(String msg, long expectedBegin, long expectedEnd, long actualBegin, long actualEnd) {
 		return String.format("%s: non overlapping intervals: [%d-%d] is outside legal space [%d,%d]", ensureMsg(msg),
 				_long(actualBegin), _long(actualEnd), _long(expectedBegin), _long(expectedEnd));
 	}
 
-	public static String incompatibleFoundationLayersMessage(String msg, Object item0, Object item1, Object fLayer0, Object fLayer1) {
+	public static String incompatibleFoundationLayers(String msg, Object item0, Object item1, Object fLayer0, Object fLayer1) {
 		return String.format("%s: expected identical foundation layers for items %s and %s - got %s and %s", ensureMsg(msg),
 				item0, item1, fLayer0, fLayer1);
 	}
