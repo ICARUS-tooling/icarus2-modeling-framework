4de88e6f8c08eb6566e3a3d7b9381b726ca81e93
Markus Gärtner <mcgaerty@ikarus-systems.net>
2019-03-29 19:15:20 +0100

    Rework BucketSetBuilderBenchmark to use the TestUtils helper for Runner.

--- icarus2-model-api/src/jmh/java/de/ims/icarus2/model/api/driver/indices/standard/BucketSetBuilderBenchmark.java
+++ icarus2-model-api/src/jmh/java/de/ims/icarus2/model/api/driver/indices/standard/BucketSetBuilderBenchmark.java
@@ -19,20 +19,22 @@
  */
 package de.ims.icarus2.model.api.driver.indices.standard;
 
-import static de.ims.icarus2.test.TestUtils.K100;
+import static de.ims.icarus2.test.TestUtils.M100;
+import static de.ims.icarus2.test.TestUtils.jmhOptions;
 import static de.ims.icarus2.test.TestUtils.randomLongs;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.AuxCounters;
 import org.openjdk.jmh.annotations.AuxCounters.Type;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
@@ -42,10 +44,11 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.BenchmarkParams;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 
 import de.ims.icarus2.model.api.driver.indices.IndexValueType;
 import de.ims.icarus2.model.api.driver.indices.standard.IndexCollectorFactory.BucketSetBuilder;
@@ -57,10 +60,10 @@ import de.ims.icarus2.model.api.driver.indices.standard.IndexCollectorFactory.Bu
 @State(Scope.Thread)
 @AuxCounters(Type.EVENTS)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Fork(1)
 public class BucketSetBuilderBenchmark {
 
 	// Benchmark parameters
+
 	@Param({"100000"})
 	private int chunkSize;
 
@@ -80,8 +83,7 @@ public class BucketSetBuilderBenchmark {
 
 	@Setup(Level.Trial)
 	public void prepareData(BenchmarkParams params) {
-		size = Math.max(params.getMeasurement().getBatchSize(),
-				params.getWarmup().getBatchSize());
+		size = params.getOpsPerInvocation();
 		if(size<=1)
 			throw new IllegalStateException("Expecting batch size!!");
 
@@ -97,8 +99,6 @@ public class BucketSetBuilderBenchmark {
 	@TearDown(Level.Iteration)
 	public void tearDown(Blackhole bh) {
 		bh.consume(builder.build());
-//		System.out.println();
-//		builder.printStats(System.out);
 	}
 
 	// AUX COUNTERS
@@ -127,35 +127,57 @@ public class BucketSetBuilderBenchmark {
 
 	@Benchmark
 	@BenchmarkMode({Mode.SingleShotTime})
-	@Measurement(iterations = 5, batchSize = K100)
-	@Warmup(iterations = 5, batchSize = K100)
-	@OperationsPerInvocation(K100)
-	public void addSingleIncrement_100K() {
-		builder.add(index++);
+	@Measurement(iterations = 5)
+	@Warmup(iterations = 5)
+	public void addSingleIncrement(Blackhole bh) {
+		for (int i = 0; i < size; i++) {
+			builder.add(index++);
+		}
+		bh.consume(builder);
 	}
 
 	@Benchmark
 	@BenchmarkMode({Mode.SingleShotTime})
-	@Measurement(iterations = 5, batchSize = K100)
-	@Warmup(iterations = 5, batchSize = K100)
-	@OperationsPerInvocation(K100)
-	public void addSingleRandom_100K() {
-		builder.add(randomData[index++]);
+	@Measurement(iterations = 5)
+	@Warmup(iterations = 5)
+	public void addSingleRandom(Blackhole bh) {
+		for (int i = 0; i < size; i++) {
+			builder.add(randomData[index++]);
+		}
+		bh.consume(builder);
+	}
+
+	private static void prepareChunkSizes(ChainedOptionsBuilder builder, int max) {
+		int current = 10_000;
+		List<String> tmp = new ArrayList<>();
+		while(current>0 && current<=max) {
+			tmp.add(String.valueOf(current));
+			current *= 10;
+		}
+		if(current>0 && current<max) {
+			tmp.add(String.valueOf(tmp));
+		}
+
+		builder.param("chunkSize", tmp.toArray(new String[tmp.size()]));
 	}
 
 	public static void main(String[] args) throws RunnerException {
-		new Runner(new OptionsBuilder()
-				.include(BucketSetBuilderBenchmark.class.getSimpleName())
-				.jvmArgsPrepend("-Djmh.separateClasspathJAR=true", "-Xmx8g", "-Xms8g")
-				.resultFormat(ResultFormatType.LATEX)
-				.shouldDoGC(true)
-				.shouldFailOnError(true)
+		final int size = M100;
 
-				.param("chunkSize", "10000")
+		ChainedOptionsBuilder builder =
+				jmhOptions(BucketSetBuilderBenchmark.class, false, ResultFormatType.CSV)
+				.jvmArgsAppend("-Xmx8g", "-Xms8g")
+
+				.operationsPerInvocation(size)
+
 				.param("useLastHitCache", "true")
-				.param("indexValueType", "INTEGER")
+				.param("indexValueType", "INTEGER");
 
-				.build())
-		.run();
+		prepareChunkSizes(builder, size);
+
+
+		Collection<RunResult> results = new Runner(builder.build()).run();
+
+		System.out.println(results);
 	}
 }
