38a5a7b85fcfa4f8dc7e0311b250a9533a190199
Markus Gärtner <mcgaerty@ikarus-systems.net>
2019-05-16 17:25:25 +0200

    Update license headers in new files.

--- icarus2-model-api/src/test/java/de/ims/icarus2/model/api/view/paged/CorpusModelTest.java
+++ icarus2-model-api/src/test/java/de/ims/icarus2/model/api/view/paged/CorpusModelTest.java
@@ -1,3 +1,19 @@
+/*
+ * ICARUS2 Corpus Modeling Framework
+ * Copyright (C) 2014-2019 Markus Gärtner <markus.gaertner@ims.uni-stuttgart.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  *
  */
@@ -5,6 +21,7 @@ package de.ims.icarus2.model.api.view.paged;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import de.ims.icarus2.util.ChangeableTest;
@@ -14,7 +31,8 @@ import de.ims.icarus2.util.PartTest;
  * @author Markus Gärtner
  *
  */
-interface CorpusModelTest<M extends CorpusModel>
+@Disabled //TODO enable and implement tests
+public interface CorpusModelTest<M extends CorpusModel>
 		extends PartTest<PagedCorpusView, M>, ChangeableTest<M> {
 
 	/**
---
+++ icarus2-model-defaults/src/test/java/de/ims/icarus2/model/standard/view/paged/DefaultCorpusModelTest.java
@@ -0,0 +1,59 @@
+/*
+ * ICARUS2 Corpus Modeling Framework
+ * Copyright (C) 2014-2019 Markus Gärtner <markus.gaertner@ims.uni-stuttgart.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package de.ims.icarus2.model.standard.view.paged;
+
+import de.ims.icarus2.model.api.view.paged.CorpusModelTest;
+import de.ims.icarus2.model.api.view.paged.PagedCorpusView;
+import de.ims.icarus2.test.TestSettings;
+
+/**
+ * @author Markus Gärtner
+ *
+ */
+class DefaultCorpusModelTest implements CorpusModelTest<DefaultCorpusModel> {
+
+	/**
+	 * @see de.ims.icarus2.util.PartTest#createEnvironment()
+	 */
+	@Override
+	public PagedCorpusView createEnvironment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see de.ims.icarus2.test.Testable#createTestInstance(de.ims.icarus2.test.TestSettings)
+	 */
+	@Override
+	public DefaultCorpusModel createTestInstance(TestSettings settings) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see de.ims.icarus2.util.ChangeableTest#invokeChange(de.ims.icarus2.util.Changeable)
+	 */
+	@Override
+	public void invokeChange(DefaultCorpusModel instance) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
