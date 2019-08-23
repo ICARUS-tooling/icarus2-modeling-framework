7193df98d7ffb47afb54929585534c967cef54a5
Markus Gärtner <mcgaerty@ikarus-systems.net>
2019-08-24 01:56:52 +0200

    Add explicit check against index values being out of bounds.

--- icarus2-model-api/src/main/java/de/ims/icarus2/model/api/driver/indices/standard/SpanIndexSet.java
+++ icarus2-model-api/src/main/java/de/ims/icarus2/model/api/driver/indices/standard/SpanIndexSet.java
@@ -17,9 +17,11 @@
 package de.ims.icarus2.model.api.driver.indices.standard;
 
 import de.ims.icarus2.GlobalErrorCode;
+import de.ims.icarus2.model.api.ModelErrorCode;
 import de.ims.icarus2.model.api.ModelException;
 import de.ims.icarus2.model.api.driver.indices.IndexSet;
 import de.ims.icarus2.model.api.driver.indices.IndexValueType;
+import de.ims.icarus2.model.manifest.util.Messages;
 import de.ims.icarus2.util.lang.ClassUtils;
 
 /**
@@ -60,6 +62,9 @@ public class SpanIndexSet implements IndexSet {
 	 */
 	@Override
 	public long indexAt(int index) {
+		if(index<0 || index>=size())
+			throw new ModelException(ModelErrorCode.MODEL_INDEX_OUT_OF_BOUNDS,
+					Messages.indexOutOfBounds("Invalid index value", 0, size()-1, index));
 		return minValue+index;
 	}
 
