da25c86d4d50f4ff99fa69feaccae6be8762724e
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-01-13 11:09:33 +0100

    Add tests for flat constraint/node statements.

---
+++ icarus2-query-api/src/test/java/de/ims/icarus2/query/api/iql/IQLFlatStatementTest.java
@@ -0,0 +1,88 @@
+/**
+ *
+ */
+package de.ims.icarus2.query.api.iql;
+
+import static de.ims.icarus2.query.api.iql.IQLTestUtils.assertParsedTree;
+import static de.ims.icarus2.query.api.iql.IQLTestUtils.f1Tree;
+import static de.ims.icarus2.query.api.iql.IQLTestUtils.randomExpressions;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import de.ims.icarus2.query.api.iql.antlr.IQL_TestParser;
+import de.ims.icarus2.test.annotations.RandomizedTest;
+import de.ims.icarus2.test.random.RandomGenerator;
+
+/**
+ * @author Markus Gärtner
+ *
+ */
+public class IQLFlatStatementTest {
+
+	private static void assertParsedStatament(String statement, String expected, String desc) {
+		assertParsedTree(statement, expected, desc, IQL_TestParser::standaloneFlatStatement, true);
+	}
+
+	private static DynamicTest makeStatementTreeTest(String statement, String expected, String desc) {
+		return dynamicTest(desc+": "+statement+"  ->  "+expected, () ->
+			assertParsedStatament(statement, expected, desc));
+	}
+
+	@RandomizedTest
+	@TestFactory
+	Stream<DynamicNode> testFlatConstraint(RandomGenerator rng) {
+		return randomExpressions(rng, 10).stream().map(pExp -> makeStatementTreeTest(
+				pExp.first, f1Tree("[{1}]", pExp), pExp.second));
+	}
+
+	@CsvSource({
+		"[], [[[\\[][\\]]]], single empty node",
+		"[] [], [[\\[\\]][\\[\\]]], two empty nodes",
+	})
+	@ParameterizedTest
+	void testEmptyNodes(String statement, String expected, String desc) {
+		assertParsedStatament(statement, expected, desc);
+	}
+
+	@CsvSource({
+		"[$a:], [[[\\[][$a:][\\]]]], single named node",
+		"[$a:] [$b:], [[\\[$a:\\]][\\[$b:\\]]], two named nodes",
+	})
+	@ParameterizedTest
+	void testNamedNodes(String statement, String expected, String desc) {
+		assertParsedStatament(statement, expected, desc);
+	}
+
+	@CsvSource({
+		"1[], 				[[[1][\\[][\\]]]], 					specifically quantified node",
+		"2+[], 				[[[2+][\\[][\\]]]], 				lower bound quantified node",
+		"4-[], 				[[[4-][\\[][\\]]]],					upper bound quantified node",
+		"<2+>[], 			[[[<2+>][\\[][\\]]]], 				lower bound quantified node",
+		"<4->[], 			[[[<4->][\\[][\\]]]], 				upper bound quantified node",
+		"<4-|6..10|13+>[], 	[[[<4-|6..10|13+>][\\[][\\]]]], 	complex quantified node",
+		"*[], 				[[[*][\\[][\\]]]], 					universally quantified node",
+		"all[], 			[[[all][\\[][\\]]]], 				universally quantified node",
+		"![], 				[[[!][\\[][\\]]]], 					existentially negated node",
+	})
+	@ParameterizedTest
+	void testQuantifiedEmptyNodes(String statement, String expected, String desc) {
+		assertParsedStatament(statement, expected, desc);
+	}
+
+	@CsvSource({
+		"[a>3], [[[\\[][a>3][\\]]]], single node with simple constraint",
+		"[a>3 && pos~\"N[NP]\"], [[[\\[][a>3&&pos~\"N\\[NP\\]\"][\\]]]], single node with complex constraint",
+		"[pos==\"NN\"] [chars()<7], [[\\[pos==\"NN\"\\]][\\[chars()<7\\]]], two nodes with constraints",
+	})
+	@ParameterizedTest
+	void testNodesWithInnerConstraints(String statement, String expected, String desc) {
+		assertParsedStatament(statement, expected, desc);
+	}
+}
