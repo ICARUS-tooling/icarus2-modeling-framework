fad366ea950bdf05cd5bd7286841feca8d595f61
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-02-12 22:04:32 +0100

    Add utility class for additional Expression implementations.

---
+++ icarus2-query-api/src/main/java/de/ims/icarus2/query/api/eval/Expressions.java
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+package de.ims.icarus2.query.api.eval;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Utility implementations of various {@link Expression} interfaces that don't fit into
+ * the thematically named other utility factories.
+ *
+ * @author Markus Gärtner
+ *
+ */
+public class Expressions {
+
+
+	public static <T> Expression<T> constant(T value) {
+		return new ConstantValue<>(value);
+	}
+
+	static final class ConstantValue<T> implements Expression<T> {
+
+		private final T value;
+		private final TypeInfo type;
+
+		ConstantValue(T value) {
+			this.value = requireNonNull(value);
+			type = TypeInfo.of(value.getClass());
+		}
+
+		@Override
+		public TypeInfo getResultType() { return type; }
+
+		@Override
+		public T compute() { return value; }
+
+		@Override
+		public Expression<T> duplicate(EvaluationContext context) { return this; }
+
+		@Override
+		public boolean isConstant() { return true; }
+	}
+}
