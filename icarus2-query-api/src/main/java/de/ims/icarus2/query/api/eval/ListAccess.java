ac0970fecb0dc1abecd95ebd11b94f8ee9fbf012
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-02-21 15:19:40 +0100

    Update license headers and adjust parameterization for expressions.

--- icarus2-query-api/src/main/java/de/ims/icarus2/query/api/eval/ListAccess.java
+++ icarus2-query-api/src/main/java/de/ims/icarus2/query/api/eval/ListAccess.java
@@ -1,3 +1,19 @@
+/*
+ * ICARUS2 Corpus Modeling Framework
+ * Copyright (C) 2014-2020 Markus Gärtner <markus.gaertner@ims.uni-stuttgart.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  *
  */
@@ -35,6 +51,7 @@ public final class ListAccess {
 	// GENERIC METHODS
 
 	/** Wraps a collection of individual expressions into a single list expression */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static ListExpression<?, ?> wrap(Expression<?>[] source) {
 		requireNonNull(source);
 		checkArgument("Source expressions array must not be empty", source.length>0);
@@ -49,7 +66,7 @@ public final class ListAccess {
 			return new IntegerListWrapper(EvaluationUtils.ensureInteger(source));
 		}
 
-		return new ObjectWrapper<>(source);
+		return new ObjectWrapper(source);
 	}
 
 	/** Unwraps a list expression into an array of individual expressions */
