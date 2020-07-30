65e2a0f5534f9eab90dd449e44ec389cbacf027a
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-07-30 21:08:05 +0200

    Move memoization layer from dedicated cache into NodeMatcher.

---
+++ icarus2-query-api/src/main/java/de/ims/icarus2/query/api/engine/matcher/NodeMatcher.java
@@ -0,0 +1,173 @@
+/*
+ * ICARUS2 Corpus Modeling Framework
+ * Copyright (C) 2014-2020 Markus Gärtner <markus.gaertner@ims.uni-stuttgart.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package de.ims.icarus2.query.api.engine.matcher;
+
+import static de.ims.icarus2.util.Conditions.checkArgument;
+import static de.ims.icarus2.util.lang.Primitives.strictToInt;
+import static java.util.Objects.requireNonNull;
+
+import java.util.Arrays;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import de.ims.icarus2.model.api.members.item.Item;
+import de.ims.icarus2.query.api.exp.Assignable;
+import de.ims.icarus2.query.api.exp.Expression;
+
+/**
+ * Matches a single node in the structural constraints of a query
+ * against potential target items. This implementation has an
+ * integrated cache function to employ memoization in order to
+ * prevent redundant calls to the actual constraint expression.
+ *
+ * @author Markus Gärtner
+ *
+ */
+public class NodeMatcher extends AbstractMatcher<Item> {
+
+	private static final int DEFAULT_SIZE = 1<<6;
+
+	private static final int BLOCK_MASK = (1<<5)-1;
+	private static final int BLOCK_POWER = 5;
+
+	private static final long ENTRY_MASK = (1<<2)-1;
+
+	private static final long EVAL_FLAG = 2;
+	private static final long RESULT_FLAG = 1;
+
+	private static final long RESULT_YES = 1;
+	private static final long RESULT_NO = 0;
+
+	/** Storage to put new items for evaluation */
+	private final Assignable<? extends Item> element;
+	/** Constraint to be evaluated */
+	private final Expression<?> constraint;
+
+	/**
+	 * Memoization cache.
+	 *
+	 * Each entry is stored as 2 bits with the higher bit signaling if the entry has
+	 * already been computed and the lower bit holding the actual result.
+	 *
+	 * Each "block" in the cache holds 32 entries and with the initial default
+	 * size of 64 this leaves room for 2048 entries before the cache needs to be resized.
+	 */
+	private long[] cache;
+
+	public NodeMatcher(int id, Assignable<? extends Item> element, Expression<?> constraint) {
+		super(id);
+
+		requireNonNull(element);
+		requireNonNull(constraint);
+
+		checkArgument("Constraint must be of type BOOLEAN", constraint.isBoolean());
+
+		this.constraint = constraint;
+		this.element = element;
+
+		cache = new long[DEFAULT_SIZE];
+	}
+
+	private long getBlock(int index) {
+		int block = index >> BLOCK_POWER;
+		if(block >= cache.length) {
+			cache = Arrays.copyOf(cache, Math.max(block, cache.length*2));
+		}
+		return cache[block];
+	}
+
+	private static long getEntry(long block, int index) {
+		int slot = (index & BLOCK_MASK) << 1;
+		return (block >> slot) & ENTRY_MASK;
+	}
+
+	private static boolean isValid(long entry) {
+		return (entry & EVAL_FLAG) == EVAL_FLAG;
+	}
+
+	private static boolean getResult(long entry) {
+		return (entry & RESULT_FLAG) == RESULT_YES;
+	}
+
+	private void updateBlock(long block, int index, boolean value) {
+		long entry = EVAL_FLAG | (value ? RESULT_YES : RESULT_NO);
+		int slot = (index & BLOCK_MASK) << 1;
+		block |= (entry<<slot);
+		cache[index >> BLOCK_POWER] = block;
+	}
+
+	@Override
+	public boolean matches(long index, Item target) {
+		requireNonNull(target);
+		int idx = strictToInt(index);
+
+		long block = getBlock(idx);
+		long entry = getEntry(block, idx);
+
+		// If possible use cached result
+		if(isValid(entry)) {
+			return getResult(entry);
+		}
+
+		// Prepare context for current target item (cheap op)
+		element.assign(target);
+
+		// Now evaluate constraint (expensive op)
+		boolean value = constraint.computeAsBoolean();
+
+		// Remember result
+		updateBlock(block, idx, value);
+
+		return value;
+	}
+	public void resetCache() {
+		//TODO maybe we should remember the maximum index we stored so that we don't need to reset all
+		Arrays.fill(cache, 0L);
+	}
+
+	@VisibleForTesting
+	boolean hasEntry(int index) {
+		int block = index >> BLOCK_POWER;
+		if(block >= cache.length) {
+			return false;
+		}
+
+		long entry = getEntry(cache[block], index);
+		return isValid(entry);
+	}
+
+	@VisibleForTesting
+	boolean getCachedValue(int index) {
+		int block = index >> BLOCK_POWER;
+		if(block >= cache.length) {
+			return false;
+		}
+
+		long entry = getEntry(cache[block], index);
+		return isValid(entry) && getResult(entry);
+	}
+
+	/** Total number of available cache entries */
+	@VisibleForTesting
+	int cacheSize() {
+		return cache.length * 32;
+	}
+
+}
---
+++ icarus2-query-api/src/test/java/de/ims/icarus2/query/api/engine/matcher/NodeMatcherTest.java
@@ -0,0 +1,182 @@
+/**
+ *
+ */
+package de.ims.icarus2.query.api.engine.matcher;
+
+import static de.ims.icarus2.model.api.ModelTestUtils.mockItem;
+import static de.ims.icarus2.util.lang.Primitives._boolean;
+import static de.ims.icarus2.util.lang.Primitives._int;
+import static de.ims.icarus2.util.lang.Primitives.strictToInt;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+import de.ims.icarus2.model.api.members.item.Item;
+import de.ims.icarus2.model.standard.members.item.DefaultItem;
+import de.ims.icarus2.query.api.exp.Assignable;
+import de.ims.icarus2.query.api.exp.Expression;
+import de.ims.icarus2.test.ApiGuardedTest;
+import de.ims.icarus2.test.TestSettings;
+import de.ims.icarus2.test.annotations.Provider;
+import de.ims.icarus2.test.annotations.RandomizedTest;
+import de.ims.icarus2.test.random.RandomGenerator;
+import de.ims.icarus2.util.Mutable.MutableObject;
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
+
+/**
+ * @author Markus Gärtner
+ *
+ */
+class NodeMatcherTest implements ApiGuardedTest<NodeMatcher> {
+
+	/**
+	 * @see de.ims.icarus2.test.TargetedTest#getTestTargetClass()
+	 */
+	@Override
+	public Class<?> getTestTargetClass() { return NodeMatcher.class; }
+
+	/**
+	 * @see de.ims.icarus2.test.Testable#createTestInstance(de.ims.icarus2.test.TestSettings)
+	 */
+	@SuppressWarnings("boxing")
+	@Override
+	public NodeMatcher createTestInstance(TestSettings settings) {
+		Assignable<? extends Item> element = mock(Assignable.class);
+		Expression<?> constraint = mock(Expression.class);
+		when(constraint.isBoolean()).thenReturn(Boolean.TRUE);
+
+		return settings.process(new NodeMatcher(1, element, constraint));
+	}
+
+	@Test
+	void testUnknown() {
+		NodeMatcher matcher = create();
+		for (int i = 0; i < matcher.cacheSize(); i++) {
+			assertThat(matcher.hasEntry(i)).isFalse();
+		}
+	}
+
+	@Provider
+	private NodeMatcher create(Item...items) {
+		final MutableObject<?> current = new MutableObject<>();
+		final Set<Item> lookup = new ReferenceOpenHashSet<>();
+		Collections.addAll(lookup, items);
+
+		Assignable<? extends Item> element = mock(Assignable.class);
+		doAnswer(invoc -> {
+			current.set(invoc.getArgument(0));
+			return null;
+		}).when(element).assign(any());
+
+		Expression<?> constraint = mock(Expression.class);
+		when(_boolean(constraint.isBoolean())).thenReturn(Boolean.TRUE);
+		when(_boolean(constraint.computeAsBoolean())).thenAnswer(invoc -> {
+			Item item = (Item) current.get();
+			return _boolean(item!=null && lookup.contains(item));
+		});
+
+		return new NodeMatcher(1, element, constraint);
+	}
+
+	@Test
+	@RandomizedTest
+	void testRememberFalse(RandomGenerator rng) {
+		Item item = mockItem();
+		NodeMatcher matcher = create();
+		int index = rng.nextInt(matcher.cacheSize());
+
+		assertThat(matcher.matches(index, item)).isFalse();
+		assertThat(matcher.hasEntry(index)).isTrue();
+		assertThat(matcher.getCachedValue(index)).isFalse();
+	}
+
+	@Test
+	@RandomizedTest
+	void testRememberTrue(RandomGenerator rng) {
+		Item item = mockItem();
+		NodeMatcher matcher = create(item);
+		int index = rng.nextInt(matcher.cacheSize());
+
+		assertThat(matcher.matches(index, item)).isTrue();
+		assertThat(matcher.hasEntry(index)).isTrue();
+		assertThat(matcher.getCachedValue(index)).isTrue();
+	}
+
+	@Test
+	void testGrowCache() {
+		NodeMatcher matcher = create();
+		int initialSize = matcher.cacheSize();
+
+		matcher.matches(matcher.cacheSize()+1, mockItem());
+
+		assertThat(matcher.cacheSize())
+			.as("Cache did not grow")
+			.isGreaterThan(initialSize);
+	}
+
+	@Test
+	@RandomizedTest
+	void testReset(RandomGenerator rng) {
+		NodeMatcher matcher = create();
+		int index = rng.nextInt(matcher.cacheSize());
+		Item item = mockItem();
+
+		matcher.matches(index, item);
+		assertThat(matcher.hasEntry(index)).isTrue();
+
+		matcher.resetCache();
+		assertThat(matcher.hasEntry(index)).isFalse();
+	}
+
+	@Test
+	@RandomizedTest
+	void testFull(RandomGenerator rng) {
+		int size = 1000;
+
+		final boolean[] data = rng.randomBooleans(size);
+
+		Item[] items = IntStream.range(0, size)
+				.mapToObj(DefaultItem::new)
+				.toArray(Item[]::new);
+
+		Item[] contained = Stream.of(items)
+				.filter(item -> data[strictToInt(item.getId())])
+				.toArray(Item[]::new);
+
+		NodeMatcher matcher = create(contained);
+
+		// First pass -> fill everything
+		for(int index = 0; index < size; index++) {
+			assertThat(matcher.hasEntry(index))
+				.as("Entry at index %d already occupied", _int(index))
+				.isFalse();
+			assertThat(matcher.matches(index, items[index]))
+				.as("Matcher produced unexpected cache entry for index %d", _int(index))
+				.isEqualTo(data[index]);
+		}
+
+		// Second path -> ensure consistency
+		for(int index = 0; index < size; index++) {
+			assertThat(matcher.hasEntry(index))
+				.as("Entry at index %d still empty", _int(index))
+				.isTrue();
+			// Make sure correct value is stored
+			assertThat(matcher.getCachedValue(index))
+				.as("Entry at index %d corrupted", _int(index))
+				.isEqualTo(data[index]);
+			// Make sure it stays the same
+			assertThat(matcher.matches(index, items[index]))
+				.as("Inconsistent result at index %d", _int(index))
+				.isEqualTo(data[index]);
+		}
+	}
+}
