4ac6d7f9951f62b2208130e152cf12bfcb0c29c1
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-08-04 13:41:42 +0200

    Add package-info for marker package.

---
+++ icarus2-query-api/src/main/java/de/ims/icarus2/query/api/engine/matcher/mark/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides marker expressions for horizontal and vertical restriction of a target node's position.
+ * <p>
+ * Note that the classes in this package also translate from IQL value space, which uses {@code 1}
+ * as first index to the range semantics of the actual matcher implementations, which all work
+ * 0-based.
+ *
+ * @author Markus Gärtner
+ *
+ */
+package de.ims.icarus2.query.api.engine.matcher.mark;
