3a1c0828c21211e0bcf4343ce4dc58a4d3bf6a07
Markus Gärtner <mcgaerty@ikarus-systems.net>
2020-11-02 23:26:02 +0100

    Rework array argument conversion facilities.

---
+++ icarus2-test-utils/src/main/java/de/ims/icarus2/test/annotations/IntMatrixArg.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package de.ims.icarus2.test.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.jupiter.params.converter.ConvertWith;
+
+import de.ims.icarus2.test.util.convert.ArrayConverters;
+
+/**
+ * @author Markus Gärtner
+ *
+ */
+@Documented
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@ConvertWith(ArrayConverters.IntegerMatrixConverter.class)
+public @interface IntMatrixArg {
+	// marker annotation
+}
--- icarus2-test-utils/src/main/java/de/ims/icarus2/test/util/convert/ArrayConverters.java
+++ icarus2-test-utils/src/main/java/de/ims/icarus2/test/util/convert/ArrayConverters.java
@@ -21,6 +21,8 @@ package de.ims.icarus2.test.util.convert;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Array;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.ParameterContext;
@@ -30,6 +32,7 @@ import org.junit.platform.commons.util.AnnotationUtils;
 
 import de.ims.icarus2.test.annotations.ArrayArg;
 import de.ims.icarus2.test.annotations.ArrayFormat;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 /**
  * @author Markus Gärtner
@@ -37,32 +40,32 @@ import de.ims.icarus2.test.annotations.ArrayFormat;
  */
 public class ArrayConverters {
 
-	static abstract class ArrayConverterBase implements ArgumentConverter {
+	@ArrayFormat
+	private static final Object _annoCatch = new Object();
 
-		@ArrayFormat
-		private static final Object _annoCatch = new Object();
-
-		private static final ArrayFormat defaultFormat;
-		static {
-			AnnotatedElement element;
-			try {
-				element = ArrayConverterBase.class.getDeclaredField("_annoCatch");
-			} catch (NoSuchFieldException | SecurityException e) {
-				throw new InternalError(e);
-			}
-			defaultFormat = AnnotationUtils.findAnnotation(element, ArrayFormat.class)
-					.orElseThrow(() -> new InternalError("Missing our default annotation field"));
+	private static final ArrayFormat defaultFormat;
+	static {
+		AnnotatedElement element;
+		try {
+			element = ArrayConverters.class.getDeclaredField("_annoCatch");
+		} catch (NoSuchFieldException | SecurityException e) {
+			throw new InternalError(e);
 		}
+		defaultFormat = AnnotationUtils.findAnnotation(element, ArrayFormat.class)
+				.orElseThrow(() -> new InternalError("Missing our default annotation field"));
+	}
+
+	public static ArrayFormat defaultFormat() { return defaultFormat; }
+
+
+	static abstract class ArrayConverterBase implements ArgumentConverter {
 
 		protected abstract Object convert(String[] items, ParameterContext context,
 				Class<?> componentType) throws ArgumentConversionException;
 
-		/**
-		 * @see org.junit.jupiter.params.converter.ArgumentConverter#convert(java.lang.Object, org.junit.jupiter.api.extension.ParameterContext)
-		 */
 		@Override
 		public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
-			final ArrayFormat format = context.findAnnotation(ArrayFormat.class).orElse(defaultFormat);
+			final ArrayFormat format = context.findAnnotation(ArrayFormat.class).orElse(defaultFormat());
 
 			if(!String.class.isInstance(source))
 				throw new ArgumentConversionException("Not a String source: "+source);
@@ -103,7 +106,102 @@ public class ArrayConverters {
 
 			return convert(items, context, componentType);
 		}
+	}
 
+	static abstract class MatrixConverterBase implements ArgumentConverter {
+
+		protected abstract Object convert(String raw) throws ArgumentConversionException;
+
+		@Override
+		public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
+			final ArrayFormat format = context.findAnnotation(ArrayFormat.class).orElse(defaultFormat());
+
+			if(!String.class.isInstance(source))
+				throw new ArgumentConversionException("Not a String source: "+source);
+
+			final Class<?> targetType = context.getParameter().getType();
+
+			if(!targetType.isArray())
+				throw new ArgumentConversionException("Not an array type parameter: "+targetType);
+
+			final Class<?> componentType = targetType.getComponentType();
+
+			String payload = (String)source;
+
+			if(payload==null || payload.isEmpty() || payload.equals(format.empty())) {
+				return Array.newInstance(componentType, 0);
+			}
+			payload = payload.trim();
+
+			final List<Class<?>> types = new ObjectArrayList<>();
+			{
+				Class<?> arrayType = targetType;
+				while(arrayType.isArray()) {
+					types.add(arrayType);
+					arrayType = arrayType.getComponentType();
+				}
+			}
+
+			final int dimensions = types.size();
+			final char open = fetch(format.open(), "opening");
+			final char close = fetch(format.close(), "closing");
+			final char del = fetch(format.delimiter(), "delimiter");
+
+			final List<Object>[] buffer = IntStream.range(0, dimensions)
+					.mapToObj(i -> new ObjectArrayList<>())
+					.toArray(List[]::new);
+			StringBuilder sb = new StringBuilder();
+
+			Object result = null;
+			int depth = -1;
+
+			//TODO handle empty sub-arrays ("-" marker)
+
+			for (int i = 0; i < payload.length(); i++) {
+				char c = payload.charAt(i);
+				if(Character.isWhitespace(c)) {
+					// Ignore whitespaces entirely
+					continue;
+				} else if(c==open) {
+					// Begin new array
+					depth++;
+				} else if(c==close || c==del) {
+					// Finish element
+					if(sb.length()>0) {
+						buffer[depth].add(convert(sb.toString().trim()));
+						sb.setLength(0);
+					}
+					if(c==close) {
+						// Finish array
+						List<Object> list = buffer[depth];
+						Object array = Array.newInstance(types.get(depth).getComponentType(), list.size());
+						for (int j = 0; j < list.size(); j++) {
+							Array.set(array, j, list.get(j));
+						}
+						if(--depth<0) {
+							result = array;
+						} else {
+							buffer[depth].add(array);
+						}
+					}
+				} else {
+					sb.append(c);
+				}
+			}
+
+			if(result==null)
+				throw new ArgumentConversionException("");
+
+			return result;
+		}
+
+		private char fetch(String s, String field) {
+			if(s.isEmpty())
+				throw new ArgumentConversionException("Symbol for array declaration must not be empty: '"+field+"'");
+			if(s.length()>1)
+				throw new ArgumentConversionException("Illegal array '"+field+"' symbol: "+s);
+			return s.charAt(0);
+		}
 	}
 
 	public static class GenericArrayConverter extends ArrayConverterBase {
@@ -142,6 +240,15 @@ public class ArrayConverters {
 				throws ArgumentConversionException {
 			return Stream.of(items).mapToInt(Integer::parseInt).toArray();
 		}
+	}
+
+	public static class IntegerMatrixConverter extends MatrixConverterBase {
+
+		@Override
+		protected Object convert(String raw) throws ArgumentConversionException {
+			return Integer.valueOf(raw);
+		}
+
 	}
 
 	//TODO add other converter implementations as the need arises
