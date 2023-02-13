39ee3fa7b09f06c4e6c1449c0bf8b0110135e71f
Markus Gärtner <mcgaerty@ikarus-systems.net>
2023-02-13 08:06:16 +0100

    feat: add processor for metadata outside the tabular structure

---
+++ icarus2-file-driver-utils/src/main/java/de/ims/icarus2/filedriver/schema/resolve/common/MetadataResolver.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package de.ims.icarus2.filedriver.schema.resolve.common;
+
+import de.ims.icarus2.IcarusApiException;
+import de.ims.icarus2.filedriver.schema.resolve.Resolver;
+import de.ims.icarus2.filedriver.schema.resolve.ResolverContext;
+import de.ims.icarus2.model.api.members.item.Item;
+
+/**
+ * @author Markus Gärtner
+ *
+ */
+public class MetadataResolver implements Resolver {
+
+	/**
+	 * @see de.ims.icarus2.filedriver.schema.resolve.Resolver#process(de.ims.icarus2.filedriver.schema.resolve.ResolverContext)
+	 */
+	@Override
+	public Item process(ResolverContext context) throws IcarusApiException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
